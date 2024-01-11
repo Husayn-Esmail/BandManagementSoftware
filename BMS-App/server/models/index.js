9dbfcd7d7dbbc15565552e48799b3695091e53ce
Husayn Esmail <husayn@esmail.ca>
2024-01-11 12:02:13 -0700

    resolved an error with the options in model's index.js

--- BMS-App/server/models/index.js
+++ BMS-App/server/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-  operatorsAliases: false,
+  operatorsAliases: 0, // boolean is deprecated, this means false.
 
   pool: {
     max: dbConfig.pool.max,
