b46d5db51d36a3ff8bc7e06ce47d8e0a2c574d16
Husayn Esmail <husayn@esmail.ca>
2024-03-07 22:08:38 -0700

    created a couple components but no actual work done

--- BMS-App/frontend/src/App.js
+++ BMS-App/frontend/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { CreateChord, DisplayChords } from './Components/add-chord.component.js';
+
 
 function App() {
   return (
     <div className="App"> 
+	  <CreateChord />
+	  <DisplayChords />
     </div>
   );
 }
---
+++ BMS-App/frontend/src/Components/add-chord.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function DisplayChords() {
+	return(
+		<div class="base-component">
+			<p>I show all the chords</p>
+		</div>
+	);
+
+}
+
+function CreateChord() {
+	return(
+		<div class="base-component">
+			<p>I display chords</p>
+		</div>
+	);
+}
+
+
+export { DisplayChords, CreateChord };
+
