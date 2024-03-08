2f77beab291c6b0f7a0c01c4bed683c547870fcc
Husayn Esmail <husayn@esmail.ca>
2024-03-08 12:13:54 -0700

    removed other components from app.js since navbar is rendering them

--- BMS-App/frontend/src/App.js
+++ BMS-App/frontend/src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
-import { CreateChord, ChordsList } from './Components/add-chord.component.js';
-import NavBar from './Components/navbar.component.js';
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import NavBar from './Components/navbar.component.js';
 
 function App() {
   return (
     <div className="App"> 
 	  <NavBar />
-	  <CreateChord />
-	  <ChordsList />
     </div>
   );
 }
